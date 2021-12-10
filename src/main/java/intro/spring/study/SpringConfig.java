e90c4b5d0711bdd8926db84f8a3074ebd5353f25
GyeomFka <clazziquai01@gmail.com>
2021-12-10 12:36:50 +0900

    form 완성

--- src/main/java/intro/spring/study/SpringConfig.java
+++ src/main/java/intro/spring/study/SpringConfig.java
@@ -3,10 +3,9 @@ package intro.spring.study;
 import intro.spring.study.repository.MemberRepository;
 import intro.spring.study.repository.MemoryMemberRepository;
 import intro.spring.study.service.MemberService;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration -> 스프링이 동작할때 읽어들인다.
+@Configuration //→ 스프링이 동작할때 읽어들인다.
 public class SpringConfig {
     //다만 controller는 예외적으로 우선적으로 필수적으로 넣어줘야한다.
     //@Bean
@@ -17,7 +16,8 @@ public class SpringConfig {
 
     //@Bean
     public MemberRepository memberRepository() { //상황에 따라 memberRepository 구현체가 바뀔수있다.
-        return new MemoryMemberRepository(); //구현체 -> interface new가 안된다.
-        //추후 '구현체' 만 교체해주면 된다. -> 자바설정파일로 빈등록할때의 장점.
+        return new MemoryMemberRepository(); //구현체 → interface new가 안된다.
+        //추후 '구현체' 만 교체해주면 된다. → 자바설정파일로 빈등록할때의 장점.
     }
+
 }
