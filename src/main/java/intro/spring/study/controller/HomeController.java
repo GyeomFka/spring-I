c40b0b3ed0056cbea19645fb0b8bac8a508e9e60
GyeomFka <clazziquai01@gmail.com>
2021-12-10 23:11:44 +0900

    DB연결 초기설정

---
+++ src/main/java/intro/spring/study/controller/HomeController.java
@@ -0,0 +1,14 @@
+package intro.spring.study.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/") // localhost:8080 호출
+    public String home() {
+        return "home";
+    }
+
+}
